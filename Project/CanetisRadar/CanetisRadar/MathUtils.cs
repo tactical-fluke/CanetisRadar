da59dda7fe7a3399a8aa087d24857b0302baf1e3
tactical_fluke <tacticalfluke304@gmail.com>
2020-05-24 14:28:37 +0800

    added gitignore and Mathutils (actually)

---
+++ Project/CanetisRadar/CanetisRadar/MathUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanetisRadar
+{
+	class MathUtils
+	{
+		public static T Clamp<T>(T x, T min, T max) where T : IComparable
+		{
+			if (x.CompareTo(min) < 0) return min;
+			else if (x.CompareTo(max) > 0) return max;
+			else return x;
+		}
+	}
+}
